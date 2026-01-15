76860fd395d70506563a5a046bfc2db38fbec8a2
Arpita <sharmaarpita307@gmail.com>
2026-01-15 15:36:14 +0530

    student details program

---
+++ my_details.py
@@ -0,0 +1,12 @@
+# Program to print  student details
+# Created by Arpita
+name = "Arpita Sharma"
+cours = "B.tech"
+sec = "IF"
+roll no = "23"
+interest = "Python and AIML"
+print("Student Details")
+print("Name:", Arpita Sharma)
+print("Cours:", B.tech)
+print("Sec:", IF)
+print("Interest:", Python and AIML)
